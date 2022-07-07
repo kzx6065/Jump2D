1ad8df54b29a28a9bd73e23c783b85c7742ace7c
kzx6065 <kzx6065@gmail.com>
2022-07-08 07:42:17 +0900

    캐릭터 점프 및 낙하속도 조절

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 jumpForce = new Vector2(0, 10);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
